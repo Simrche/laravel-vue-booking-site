55106725e9ddef5cbbbd4ecfdb00f3c7ba7c82c4
Simon Roche <simon.roche@edu.devinci.fr>
2022-03-16 16:14:28 +0100

    add some style using the tools of vue

---
+++ resources/js/bookables/Bookables.vue
@@ -0,0 +1,98 @@
+<template>
+    <div>
+        <div v-if="loading">
+            <p>Data is Loading ...</p>
+        </div>
+        <div v-else>
+            <div
+                class="row mb-4"
+                v-for="row in rows"
+                :key="'row' + row"
+            >
+                <div
+                    class="col"
+                    v-for="(bookable, column) in bookablesInRow(row)"
+                >
+                    <BookableListItem
+                        :title="bookable.title"
+                        :content="bookable.content"
+                        :price="1000"
+                    ></BookableListItem>
+                </div>
+                <div class="col" v-for="p in placeholdersInRow(row)" :key="'placeholder' + row + p">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import BookableListItem from "./BookableListItem"
+
+export default {
+
+    components: {
+        BookableListItem,
+    },
+    data: () => {
+        return {
+            bookables: null,
+            loading: false,
+            columns: 3,
+        }
+    },
+    computed: {
+        rows() {
+            return this.bookables === null ? 0 : Math.ceil(this.bookables.length / this.columns)
+        }
+    },
+    methods: {
+        bookablesInRow(row) {
+            return this.bookables.slice((row - 1) * this.columns, row * this.columns)
+        },
+        placeholdersInRow(row) {
+            return this.columns - this.bookablesInRow(row).length
+        }
+    },
+    created() {
+        this.loading = true
+        setTimeout(() => {
+            this.bookables = [
+                {
+                    title: "Cheap Villa",
+                    content: "A very cheap Villa"
+                },
+                {
+                    title: "Cheap Villa",
+                    content: "A very cheap Villa"
+                },
+                {
+                    title: "Cheap Villa",
+                    content: "A very cheap Villa"
+                },
+                {
+                    title: "Cheap Villa",
+                    content: "A very cheap Villa"
+                },
+                {
+                    title: "Cheap Villa",
+                    content: "A very cheap Villa"
+                },
+                {
+                    title: "Cheap Villa",
+                    content: "A very cheap Villa"
+                },
+                {
+                    title: "Cheap Villa",
+                    content: "A very cheap Villa"
+                },
+            ]
+            this.loading = false
+        }, 2000)
+    },
+    mounted() {
+        console.log('mounted')
+    },
+}
+</script>
