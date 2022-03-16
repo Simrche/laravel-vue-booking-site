06231ed75b14072303d95ea08d0b48080e1bcd4c
Simon Roche <simon.roche@edu.devinci.fr>
2022-03-16 10:37:30 +0100

    add vue-router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,16 @@
 require('./bootstrap');
 
+import router from "./routes"
+import VueRouter from "vue-router/dist/vue-router"
+import Vue from "vue";
+
 window.Vue = require('vue').default;
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
+Vue.use(VueRouter);
+
 const app = new Vue({
     el: '#app',
+    router,
 });
