417fecc4594e4813d823f6a3348148e2f7290499
Simon Roche <simon.roche@edu.devinci.fr>
2022-03-16 10:57:39 +0100

    component : global registration to local registration

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,15 +3,16 @@ require('./bootstrap');
 import router from "./routes"
 import VueRouter from "vue-router/dist/vue-router"
 import Vue from "vue";
+import Index from "./Index"
 
 window.Vue = require('vue').default;
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('example-2', require('./components/Example2.vue').default);
 
 Vue.use(VueRouter);
 
 const app = new Vue({
     el: '#app',
     router,
+    components: {
+        "index": Index
+    }
 });
