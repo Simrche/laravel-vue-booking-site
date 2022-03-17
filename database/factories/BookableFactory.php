4608755f8812560e33a0e0f8329641e3e945e67c
Simon Roche <simon.roche@edu.devinci.fr>
2022-03-17 18:42:14 +0100

    add some types of bookables in the factory

--- database/factories/BookableFactory.php
+++ database/factories/BookableFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Bookable;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 class BookableFactory extends Factory
 {
@@ -16,8 +17,10 @@ class BookableFactory extends Factory
 
     public function definition()
     {
+        $suffix = ["Villa", "House", "Cottage", "Luxury Villas", "Cheap House", "Rooms", "Cheap Rooms", "Luxury Rooms", "Fancy Rooms"];
+
         return [
-            'title' => $this->faker->city(),
+            'title' => $this->faker->city() . ' ' . Arr::random($suffix),
             'description' => $this->faker->text()
         ];
     }
