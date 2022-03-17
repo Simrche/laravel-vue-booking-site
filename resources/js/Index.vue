1c6e7e045b6ca5215189c999fadefdd4b0c7d87e
Simon Roche <simon.roche@edu.devinci.fr>
2022-03-17 23:22:56 +0100

    one bookable vue / get the bookable in the db with axios request

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -2,7 +2,7 @@
     <div>
         <nav class="navbar bg-white border-bottom navbar-light">
             <router-link class="navbar-brand mr-auto ml-4" :to="{name: 'home'}">Laravel Booking</router-link>
-            <router-link class="btn nav-button" :to="{name: 'second'}">Second</router-link>
+            <router-link class="btn nav-button" :to="{name: 'bookable'}">Bookable</router-link>
         </nav>
 
         <div class="container mt-4 mb-4 pr-4 p1-4">
