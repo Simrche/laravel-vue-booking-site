dc8631dfe3f8570e548943e36a89ac86a264d5a7
Simon Roche <simon.roche@edu.devinci.fr>
2022-03-18 20:27:14 +0100

    add laravel toolbar

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -5,21 +5,37 @@
         <div class="form-row d-flex justify-content-between">
             <div class="form-group col-md-6">
                 <label for="from">From</label>
-                <input type="text" name="from" id="from" class="form-control form-control-sm" placeholder="Start date">
+                <input @keyup.enter="check" v-model="from" type="text" name="from" id="from" class="form-control form-control-sm" placeholder="Start date">
             </div>
             <div class="form-group col-md-6">
                 <label for="to">To</label>
-                <input type="text" name="to" id="to" class="form-control form-control-sm" placeholder="End date">
+                <input @keyup.enter="check" v-model="to" type="text" name="to" id="to" class="form-control form-control-sm" placeholder="End date">
             </div>
         </div>
 
         <div class="form-row mt-3">
-            <button class="btn btn-secondary btn-block col-md-12">Check!</button>
+            <button class="btn btn-secondary btn-block col-md-12" @click="check">Check!</button>
         </div>
 
 
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            from: null,
+            to: null,
+        }
+    },
+    methods: {
+        check() {
+            alert('i will check something now')
+        }
+    }
+}
+</script>
 
 <style scoped>
     label {
