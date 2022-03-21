cc24ef33a2d9bc3fdf4f68d6789994649aab3dd3
Simon Roche <simon.roche@edu.devinci.fr>
2022-03-21 17:46:35 +0100

    add reviews controller

---
+++ app/Http/Controllers/Api/BookableReviewController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Bookable;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BookableReviewController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke($id, Request $request)
+    {
+        $bookable = Bookable::findOrFail($id);
+
+        return $bookable->review()->latest()->get();
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('bookables', 'App\Http\Controllers\Api\BookableController');
 Route::get('bookables/{bookable}/availability', 'App\Http\Controllers\Api\BookableAvailabilityController')->name('bookables.availability.show');
+Route::get('bookables/{bookable}/reviews', 'App\Http\Controllers\Api\BookableReviewController')->name('bookables.reviews.index');
