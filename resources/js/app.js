1e620aff73cd9886cab62ba3e658f1ad5419324b
Simon Roche <simon.roche@edu.devinci.fr>
2022-03-28 23:43:42 +0200

    add vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,10 +9,12 @@ import StarRating from "./shared/components/StarRating"
 import FatalError from "./shared/components/FatalError"
 import Success from "./shared/components/Success"
 import ValidationErrors from "./shared/components/ValidationErrors"
+import Vuex from 'vuex'
 
 window.Vue = require('vue').default;
 
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 Vue.filter("fromNow", value => {
     return moment(value).fromNow()
@@ -23,9 +25,21 @@ Vue.component('FatalError', FatalError)
 Vue.component('Success', Success)
 Vue.component('v-errors', ValidationErrors)
 
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment(state) {
+            state.count++
+        }
+    }
+})
+
 const app = new Vue({
     el: '#app',
     router,
+    store,
     components: {
         "index": Index
     }
