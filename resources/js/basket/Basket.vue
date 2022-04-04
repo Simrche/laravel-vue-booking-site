d9eb0ab85d3b17dcb53c1d953a19895e4489f3bf
Simon Roche <simon.roche@edu.devinci.fr>
2022-04-04 12:23:06 +0200

    making a booking in the front

--- resources/js/basket/Basket.vue
+++ resources/js/basket/Basket.vue
@@ -45,7 +45,7 @@
                 <hr>
                 <div class="row">
                     <div class="col-md-12 form-group">
-                        <button type="submit" class="btn btn-lg btn-primary btn-block w-100">
+                        <button type="submit" class="btn btn-lg btn-primary btn-block w-100" @click.prevent="book">
                             Book now!
                         </button>
                     </div>
@@ -89,10 +89,15 @@
 
 <script>
 import {mapGetters, mapState} from "vuex"
+import validationErrors from "./../shared/mixins/validationErrors"
 
 export default {
     data() {
+        mixins: {
+            [validationErrors]
+        }
         return {
+            loading: false,
             customer: {
                 first_name: null,
                 last_name: null,
@@ -110,6 +115,26 @@ export default {
         ...mapState({
             basket: state => state.basket.items
         })
+    },
+    methods: {
+        async book() {
+            this.loading = true
+
+            try {
+                await axios.post(`/api/checkout`, {
+                    customer: this.customer,
+                    bookings: this.basket.map(basketItem => ({
+                        bookable_id: basketItem.bookable.id,
+                        from: basketItem.dates.from,
+                        to: basketItem.dates.to
+                    }))
+                })
+            } catch (err) {
+
+            }
+
+            this.loading = false
+        }
     }
 }
 </script>
